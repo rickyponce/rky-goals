3187f77529fdc6ea1a01a844c000e3ffa71862b1
irishcamilleipili <ipiliirish@gmail.com>
2025-08-27 15:34:15 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwv1Hq1CJBpN4WxxuTsvLLIDb3w7rd47k",
+  authDomain: "rky-goals.firebaseapp.com",
+  projectId: "rky-goals",
+  storageBucket: "rky-goals.firebasestorage.app",
+  messagingSenderId: "633237278296",
+  appId: "1:633237278296:web:5ed55fcfd84c3b91f6442d"
+};
+
+
+const app = initializeApp(firebaseConfig);
