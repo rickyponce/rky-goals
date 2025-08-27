8fa50f92387f2288b23cc22ec3d076021e6ae4a5
irishcamilleipili <ipiliirish@gmail.com>
2025-08-27 15:41:22 +0800

    Firebase Configuration

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        R K Y  G O A L S
+        R K Y  G O A L S TRACKER
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
